1f3a363f8d413a4430678cf90621e51deb4cc51e
ketley <105372359+ketleycelest2510@users.noreply.github.com>
2024-10-09 10:43:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function name(params) {
-    
+function calcula(){
+    let preco = document.getElementById('valor').value;
+    let parce = document.getElementById('valor').value;
+
+    let endereco = document.querySelector('#aqui thead');
+    let linha_tabela = document.querySelector('#aqui tbody');
+
+let cabeca = `
+        <tr>
+            <th> parcelas</th><th>valor</th>
+        </tr>
+`;
 }
